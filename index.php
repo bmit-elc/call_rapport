cb18dc0faed16680ca94b1afdda09b5e37e6787c
Ewan Hally <ewan.hally@EDU.LAN>
2023-10-09 13:43:36 +0200

    first push

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <script src="jsdatei.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <p id="result"></p>
+    <?php
+    phpinfo();
+    ?>
+    <?php
+$xml=simplexml_load_file("TicketCollector.xml") or die("Error: Cannot create object");
+echo json_encode($xml);
+print_r($xml);
+?>
+</body>
+</html>
