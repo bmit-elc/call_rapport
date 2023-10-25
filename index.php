ca9e7b564ac61f7ca6f5b01b67ab36757b356905
Ewan Hally <ewan.hally@EDU.LAN>
2023-10-25 13:09:57 +0200

    added chart.js example

--- index.php
+++ index.php
@@ -3,26 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css" />
-    <script src="jsdatei.js" defer></script>
     <title>Document</title>
     <?php
-    $xml=simplexml_load_file("TicketCollector.xml") or die("Error: Cannot create object");
-    $jsonData = json_encode($xml);
-    
-    ?>
-    <script>
-        const jsonData = <?php echo $jsonData; ?>
-        console.log(data);
-    </script>
+        	$url = "styles.css";
+        	echo "<link rel='stylesheet' href='{$url}'>";
+        ?>
+    <script src="jsdatei.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+     
 </head>
 <body>
+  
+<?php
+    $CallAccountingList = simplexml_load_file("TicketCollector.xml") or die("Error: Cannot create object");
+    foreach ($CallAccountingList->CallAccounting as $CallAccounting){
+        if($CallAccounting->Time == '14:32:00'){
+            echo $CallAccounting->CallDuration;
+
+        }    
+    }
+    ?>
+    
+    
+    <br>
     <label for="date">Bitte selektieren Sie ein Datum:</label>
     <input type="date" placeholder="Datum" id="date" />
-    <button id="getData">Get Data</button>
+    <button id="getData">Daten holen</button>
 
-    <div id="result"></div>
+    <div id="result">
+    <canvas id="myChart"></canvas>
+    </div>
+  
+
     
-
 </body>
 </html>
--- jsdatei.js
+++ jsdatei.js
@@ -18,3 +18,24 @@ function searchXML(){
     x = xmlDoc.getElementsByTagName("DialledNumber");
     input = document.get
 }
+
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
