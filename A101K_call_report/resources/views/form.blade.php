84240d70047f9bbcc74dbc968cda795e9f45bf23
bmit-hae <113096592+bmit-hae@users.noreply.github.com>
2023-12-07 08:14:47 +0100

    Revert "welcome view anzeigen lassen"

--- A101K_call_report/resources/views/form.blade.php
+++
@@ -1,7 +0,0 @@
-
-@extends('layout')
-
-@section('content')
-    <h1>Formularseite</h1>
-    <!-- Das Formular hier -->
-@endsection
--- A101K_call_report/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-
-@section('content')
-    <h1>Willkommen auf der Startseite!</h1>
-    <!-- Weitere Inhalte hier -->
-@endsection
-
